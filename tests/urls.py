d21eaccbeb7c5d583f59e28250679debfda15ed1
Davide Ria <d.ria@frankhood.it>
2024-02-20 13:14:58 +0100

    Completed release 0.2.0

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/v1/", include("tests.example.api.urls")),
 ]
